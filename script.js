6e6a578f24725106c840119c21123ad901c4a07d
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-14 17:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+	let ans = "";
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i) != " "){
+			ans+= str.charAt(i);
+		}
+	}
+	let start = 0, end = str.length;
+	while(start < right){
+		if(ans.charAt(start) !== ans.charAt(end)){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
